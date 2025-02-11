b2cbe759a6b75f0dbddf25cd9ea442895447f769
a1exuus <162327737+a1exuus@users.noreply.github.com>
2025-02-11 16:07:58 +0300

    fixed possible errors

--- scripts.py
+++ scripts.py
@@ -1,15 +1,24 @@
 from datacenter.models import Mark, Lesson, Chastisement, Commendation, Schoolkid
 from random import choice, randint
+from django.core.exceptions import ObjectDoesNotExist, MultipleObjectsReturned
 
 
 def get_schoolkid_card(child_name):
-    child = Schoolkid.objects.get(full_name__contains=child_name)
+    try:
+        child = Schoolkid.objects.get(full_name__contains=child_name).unique()
+    except ObjectDoesNotExist:
+        raise('Такого ученика не существует!')
+    except MultipleObjectsReturned:
+        raise('Учеников с таким именем более 2-х. Попытайтесь ввести полное ФИО')
     return child
 
 
 def create_commendation(schoolkid, subject):                                                                         
     child = get_schoolkid_card(schoolkid)
-    lessons = Lesson.objects.filter(subject__title=subject)
+    try:
+        lessons = Lesson.objects.filter(subject__title=subject).order_by('-date')
+    except ObjectDoesNotExist:
+        raise('Такого урока не существует!')
     lesson = choice(lessons)
     Commendation.objects.create(text='Хвалю!', created=lesson.date, schoolkid=child, subject=lesson.subject, teacher=lesson.teacher)
 
