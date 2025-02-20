5fb9707d0c97310e200d2e7eabcdf5c7e2254aee
a1exuus <162327737+a1exuus@users.noreply.github.com>
2025-02-20 16:57:10 +0300

    Update scripts.py

--- scripts.py
+++ scripts.py
@@ -4,31 +4,25 @@ from django.core.exceptions import ObjectDoesNotExist, MultipleObjectsReturned
 
 
 def get_schoolkid_card(child_name):
-    try:
-        child = Schoolkid.objects.get(full_name__contains=child_name).unique()
-    except ObjectDoesNotExist:
-        raise('Такого ученика не существует!')
-    except MultipleObjectsReturned:
-        raise('Учеников с таким именем более 2-х. Попытайтесь ввести полное ФИО')
+    try: child = Schoolkid.objects.filter(full_name__contains=child_name).distinct().first()
+    except ObjectDoesNotExist: raise ObjectDoesNotExist
+    except MultipleObjectsReturned: raise MultipleObjectsReturned
     return child
 
 
 def create_commendation(schoolkid, subject):                                                                         
     child = get_schoolkid_card(schoolkid)
-    try:
-        lessons = Lesson.objects.filter(subject__title=subject).order_by('-date')
-    except ObjectDoesNotExist:
-        raise('Такого урока не существует!')
-    lesson = choice(lessons)
-    Commendation.objects.create(text='Хвалю!', created=lesson.date, schoolkid=child, subject=lesson.subject, teacher=lesson.teacher)
+    lessons = Lesson.objects.filter(subject__title=subject).order_by('-date')
+    if lessons:
+        lesson = choice(lessons)
+        Commendation.objects.create(text='Хвалю!', created=lesson.date, schoolkid=child, subject=lesson.subject, teacher=lesson.teacher)
+    else:
+        raise()
 
 
 def remove_chastisements(schoolkid):
-    for chastisement in Chastisement.objects.filter(schoolkid=schoolkid):
-        chastisement.delete()
+    Chastisement.objects.filter(schoolkid=schoolkid).delete()
 
 
 def fix_marks(schoolkid):
-    for mark in Mark.objects.filter(schoolkid=schoolkid, points__in=[2, 3]):
-        mark.points = randint(4, 5)
-        mark.save()
+    Mark.objects.filter(schoolkid=schoolkid, points__in=[2, 3]).update(points=randint(4, 5))
