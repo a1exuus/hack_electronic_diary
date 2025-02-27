11935fb630cc070b59c29059836c88d2e6134373
Alexey <162327737+a1exuus@users.noreply.github.com>
2025-02-27 14:11:21 +0300

    Update scripts.py

--- scripts.py
+++ scripts.py
@@ -4,20 +4,27 @@ from django.core.exceptions import ObjectDoesNotExist, MultipleObjectsReturned
 
 
 def get_schoolkid_card(child_name):
-    try: child = Schoolkid.objects.filter(full_name__contains=child_name).distinct().first()
-    except ObjectDoesNotExist: raise ObjectDoesNotExist
-    except MultipleObjectsReturned: raise MultipleObjectsReturned
+    try:
+        child = Schoolkid.objects.get(full_name__contains=child_name)
+    except ObjectDoesNotExist:
+        raise ValueError(f"Ученик с именем '{child_name}' не найден.")
+    except MultipleObjectsReturned:
+        raise ValueError(f"Найдено несколько учеников с именем '{child_name}', уточните запрос.")
+
     return child
 
 
-def create_commendation(schoolkid, subject):                                                                         
-    child = get_schoolkid_card(schoolkid)
-    lessons = Lesson.objects.filter(subject__title=subject).order_by('-date')
-    if lessons:
-        lesson = choice(lessons)
-        Commendation.objects.create(text='Хвалю!', created=lesson.date, schoolkid=child, subject=lesson.subject, teacher=lesson.teacher)
+def create_commendation(schoolkid, subject):
+    if schoolkid:                                                                    
+        child = get_schoolkid_card(schoolkid)
+        lessons = Lesson.objects.filter(subject__title=subject).order_by('-date')
+        if lessons:
+            lesson = choice(lessons)
+            Commendation.objects.create(text='Хвалю!', created=lesson.date, schoolkid=child, subject=lesson.subject, teacher=lesson.teacher)
+        else:
+            raise ValueError('Уроков по данному предсету не существует. Проверьте правильность написания названия предмета')
     else:
-        raise()
+        raise ValueError('Ученика с данным именем не существует!')
 
 
 def remove_chastisements(schoolkid):
@@ -25,4 +32,7 @@ def remove_chastisements(schoolkid):
 
 
 def fix_marks(schoolkid):
-    Mark.objects.filter(schoolkid=schoolkid, points__in=[2, 3]).update(points=randint(4, 5))
+    if schoolkid:
+        Mark.objects.filter(schoolkid=schoolkid, points__in=[2, 3]).update(points=randint(4, 5))
+    else:
+        raise ValueError(f'Данного ученика не существует!')
